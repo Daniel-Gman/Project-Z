14d02c42f7b1a9f3fd3b33ed791b3f25c2c35f9d
BenteKavsek <35726@ma-web.nl>
2023-10-19 12:40:33 +0200

    db gegevens in env file

--- source/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("SERVERNAME","db3");
-define("USERNAME","db_user");
-define("PASSWORD","mijn_p@ss#");
-define("DBNAME","voorbeeld_db");
--- source/connect.php
+++ source/connect.php
@@ -1,13 +1,17 @@
 <?php
-require("config.php");
-
 function db_connect() {
-    $servername = "db3";
-    $username = "db_user";
-    $password = "mijn_p@ss#";
-    $dbname = "voorbeeld_db";
+    $envSettings = [];
+    if (file_exists(dirname(__FILE__).'/.env')) {
+        $envSettings = parse_ini_file(dirname(__FILE__).'/.env');
+    
+
+$servername=$envSettings["DB_NAME"];
+$username= $envSettings["DB_USER"];
+$password= $envSettings["DB_PASSWORD"];
+$dbname= $envSettings["DB_HOST"];
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);    
+    }
 
     return $conn;
 }
