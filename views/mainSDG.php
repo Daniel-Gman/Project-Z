d8da241e8db65292c934c64b8b6a2c8da69ce95f
BenteKavsek <35726@ma-web.nl>
2023-11-01 10:50:33 +0100

    template bente responsive

--- views/mainSDG.php
+++ views/mainSDG.php
@@ -1,37 +1,33 @@
 <?php
 function makeTiles() {
-    
-    $numberArray = range(1, 17);
-    shuffle($numberArray);
 
-    for ($i = 0; $i < 3; $i++) {
-        $currentNumber = $numberArray[$i];
-        if($currentNumber > 8) {
+    require_once("../source/connect.php");
+    $connection = db_connect();
+
+    $sql = "SELECT * FROM SDG ORDER BY RAND() LIMIT 3";
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+
+    while($row = mysqli_fetch_assoc($result)) {
+
+        $template = "templateD.php";
+
+        if($row['id']> 8) {
+
             $template = "templateB.php";
-        } else {
-            $template = "templateD.php"; 
-        }
-        
-        require_once("../source/connect.php");
-        $connection = db_connect();
-
-        $sql = "SELECT * FROM SDG WHERE id=". $currentNumber ."";
-        $result = mysqli_query($connection, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-
-            while($row = mysqli_fetch_assoc($result)) {
-                echo '
-                <a href="'. $template .'?sdg='. $currentNumber .'">
-                    <div class="main__SDG__img__cover"></div>
-                    <div><img class="main__SDG__img" style="background:'. $row["kleur"] .';"src="img/sdg'. $currentNumber .'.svg" alt=""></div>
-                </a>';
             }
 
-        } else {
-            echo "0 results";
-        }
-
+            echo '
+            <a class="main-sdg" href="'. $template .'?sdg='. $row['id'] .'">
+                <div class="main__SDG__img__cover"></div>
+                <div><img class="main__SDG__img" style="background:'. $row["kleur"] .';"src="img/sdg'. $row['id'] .'.svg" alt=""></div>
+            </a>';
     }
+
+    } else {
+        echo "0 results";
+    }
+
 }
 
